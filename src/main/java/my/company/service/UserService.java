309eff8a5eefa5aa7f7433d68dfc6069e1038bf2
Alexander <61922116+Lazovski1991@users.noreply.github.com>
2021-09-19 23:08:21 +0300

    Update UserService.java

--- src/main/java/my/company/service/UserService.java
+++ src/main/java/my/company/service/UserService.java
@@ -4,17 +4,17 @@ import my.company.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Optional;//todo это что за импорт?
 
-@Repository
+@Repository//todo це шо?
 public interface UserService {
     void addUser(User user);
 
-    void deletedOnId(Integer id);
+    void deletedOnId(Integer id);//todo что за On?
 
     List<User> selectAll();
 
-    User selectedOnId(Integer id);
+    User selectedOnId(Integer id);//todo некае не очень название и что за On?
 
     void deletedAll();
 }
