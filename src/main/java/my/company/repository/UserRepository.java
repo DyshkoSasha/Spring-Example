8ae28d79a5d9cfc0388dc84eb8e5cc6afe91eb4a
Alexander <61922116+Lazovski1991@users.noreply.github.com>
2021-09-19 23:14:30 +0300

    Update UserRepository.java

--- src/main/java/my/company/repository/UserRepository.java
+++ src/main/java/my/company/repository/UserRepository.java
@@ -3,7 +3,8 @@ package my.company.repository;
 import my.company.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+//todo они везде
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+//todo они везде
 }
+//todo они везде
