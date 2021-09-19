4255144cce3a2ce7a96f3bb907e92aded89ad390
Alexander <61922116+Lazovski1991@users.noreply.github.com>
2021-09-19 23:12:38 +0300

    Update UserServiceImpl.java

--- src/main/java/my/company/service/UserServiceImpl.java
+++ src/main/java/my/company/service/UserServiceImpl.java
@@ -8,11 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional
+@Transactional //todo  у этой транзакции есть параметра, и для методов чтения его нужно поставить
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService{//todo почитай про кодстайл, он у тебя хромает
     private final UserRepository userRepository;
-
+//todo сейчас на всех почти проектах есть ломбок, исправь все с ломбоком
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public void deletedOnId(Integer id) {
+    public void deletedOnId(Integer id) {// todo про названия написал в интерфейсе
         userRepository.deleteById(id);
     }
 
@@ -36,13 +36,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User selectedOnId(Integer id) {
-       return userRepository.findById(id).orElse(new User());
+       return userRepository.findById(id).orElse(new User());//todo прочитай про optional и посмотри что он тебе вернет если юзера не будет с таким айди и поймешь что это гамно
     }
 
     @Override
     public void deletedAll() {
         userRepository.deleteAll();
     }
-
+//todo посмотри пробелы везде, сделай по-людски а не на скорую руку
 
 }
